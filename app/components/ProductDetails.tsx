3bea1472e1caf2828033676cb0565b34c4897de6
Borshigov <k.borshigov@accenture.com>
2025-10-16 15:19:54 +0200

    snap slider for product details mobile view

--- app/components/ProductDetails.tsx
+++ app/components/ProductDetails.tsx
@@ -36,9 +36,22 @@ export default function ProductDetails({ product }: { product: Product }) {
     }
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 space-x-4 max-w-screen-xl mx-auto mt-20">
-            <div className="flex gap-2 justify-center">
-                {/* Thumbnail column */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 max-w-screen-xl mx-auto lg:mt-20">
+            {/* Mobile: snap slider */}
+            <div className="flex overflow-x-auto snap-x snap-mandatory gap-2 lg:hidden">
+                {product.product_images.map((image, index) => (
+                <img
+                    key={index}
+                    src={image.public_url || ''}
+                    alt={`${product.name} ${index + 1}`}
+                    className="snap-center w-full flex-shrink-0 object-contain"
+                    style={{ maxHeight: '80vh' }}
+                />
+                ))}
+            </div>
+
+            {/* Thumbnail column */}
+            <div className="hidden lg:flex gap-2 justify-center">
                 <div className="flex flex-col gap-2 w-24  ">
                     {product.product_images.map((image, index) => (
                         <img
@@ -66,14 +79,14 @@ export default function ProductDetails({ product }: { product: Product }) {
                 </div>
             </div>
 
-            <div className="lg:pr-20 lg:pl-10">
+            <div className="lg:pr-20 lg:pl-10 mt-10 lg:mt-0 mx-4 lg:mx-0">
                 <h1 className="text-2xl font-medium">{product.name}</h1>
                 <p className="text-black mt-4">{product.details}</p>
-                <p className="text-black text-xl mt-10">{product.product_item[0].price} €</p>
-                <div className="flex items-center gap-2 mt-10">
+                <p className="text-black text-xl mt-8">{product.product_item[0].price} €</p>
+                <div className="flex items-center gap-2 mt-20">
                     <button 
                         onClick={handleAddToCart}
-                        className="bg-zinc-100 text-black px-4 py-2 rounded-3xl hover:bg-zinc-50 
+                        className="bg-zinc-100 text-black px-4 py-2 rounded-3xl hover:bg-zinc-50 w-full lg:w-auto
                         transition-all disabled:opacity-70 min-w-[140px] flex items-center justify-center gap-2"
                     >
                        { showSuccess ? (
